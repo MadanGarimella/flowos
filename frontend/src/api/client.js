8ad83770002f1c11f32baec2e9790a0375b964b6
Chandrashekhar Madan <madangarimella@gmail.com>
2026-05-08 13:10:03 +0530

    Frontend Changes

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL ?? "http://72.61.241.195:8080";
+export const API_URL = "http://72.61.241.195:8080";
 
 export async function request(path, options, token) {
   const headers = {
